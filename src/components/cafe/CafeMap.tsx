d9c58384777e8fb21c305fe0f6383497bde48e6d
Paz <124618933+Paz25@users.noreply.github.com>
2026-03-10 23:00:08 +0700

    Remove Leaflet and related dependencies; refactor CafeMap component to eliminate unused code and simplify structure.

--- src/components/cafe/CafeMap.tsx
+++ src/components/cafe/CafeMap.tsx
@@ -1,32 +1,6 @@
 "use client";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
 import { MapPin } from "lucide-react";
-
-const markerIcon = L.divIcon({
-  className: "",
-  html: `
-    <div style="
-        width: 36px;
-        height: 36px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background: var(--primary);
-        border-radius: 50% 50% 50% 0;
-        transform: rotate(-45deg);
-        border: 3px solid white;
-        box-shadow: 0 2px 8px rgba(0,0,0,0.3);
-    ">
-        <div style="width: 50%; aspect-ratio: 1/1; border-radius: 50%; background: white;"></div>
-    </div>
-  `,
-  iconSize: [36, 36],
-  iconAnchor: [18, 36],
-  popupAnchor: [0, -36],
-});
 
 interface CafeMapProps {
   name: string;
@@ -34,36 +8,6 @@ interface CafeMapProps {
   longitude: number;
   gmaps_link: string;
 }
-
-// export default function CafeMap({
-//   name,
-//   latitude,
-//   longitude,
-//   gmaps_link,
-// }: CafeMapProps) {
-//   return (
-//     <MapContainer
-//       center={[latitude, longitude]}
-//       zoom={17}
-//       scrollWheelZoom={false}
-//       className="w-full h-full rounded-2xl z-0"
-//     >
-//       <TileLayer
-//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
-//         url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-//       />
-//       <Marker
-//         position={[latitude, longitude]}
-//         icon={markerIcon}
-//         eventHandlers={{
-//           click: () => window.open(gmaps_link, "_blank"),
-//         }}
-//       >
-//         <Popup>{name}</Popup>
-//       </Marker>
-//     </MapContainer>
-//   );
-// }
 
 export default function CafeMap({
   name,
